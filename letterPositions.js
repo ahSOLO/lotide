3c482693cb1f314f047603909bf040dcffe0b3d1
Andrew Han <junmenghan@gmail.com>
2021-01-06 22:24:00 +0000

    implemented letterPositions

---
+++ letterPositions.js
@@ -0,0 +1,49 @@
+// Function Implementation
+const letterPositions = function(sentence) {
+  let output = {};
+  for (const [key, value] of Object.entries(sentence)) {
+    if (!output[value]) output[value] = [];
+    if (output[value] !== " ") output[value].push(Number(key));
+  }
+  console.log(output);
+  return output;
+};
+
+
+// Test Function
+// Check if arrays are equal
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  if (arr1 === [] && arr2 !== []) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+};
+
+// Check assertion that arrays are equal
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+  else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Test Case
+const obj = { 
+  l: [0],
+  i: [1, 11],
+  g: [2],
+  h: [3, 5, 15, 18],
+  t: [4, 14],
+  o: [6, 19],
+  u: [7, 20],
+  s: [8, 21],
+  e: [9, 16, 22],
+  n: [12]
+}
+
+assertArraysEqual(letterPositions("lighthouse in the house")["h"], obj["h"]);
+assertArraysEqual(letterPositions("lighthouse in the house")["e"], obj["e"]);
